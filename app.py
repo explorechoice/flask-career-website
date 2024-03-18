38e8e4f0e151130b8e7ca804a7e852d19afa7538
Rajeshwar Sharma <62470446+me-rsharma@users.noreply.github.com>
2024-03-18 17:21:00 +0000

    Added html and css files

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-  return "<h1>Hare Krishna</h1>"
+  return render_template(template_name_or_list='home.html')
 
 
 if __name__ == "__main__":
