0192b02d7c4b4c42495a3ac33a875f9e94dd363e
Rajeshwar Sharma <62470446+me-rsharma@users.noreply.github.com>
2024-03-19 06:47:56 +0000

    Added requirements.txt and job openings

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+openings = [
+  {
+    "id": 123,
+    "title": "Software Developer",
+    "location": "Pune, India",
+    "salary": "Rs. 10,00,000"
+  },
+  {
+    "id": 23,
+    "title": "ML/AI engineer",
+    "location": "Bangalore, India",
+    "salary": "Rs. 18,00,000"
+  },
+  {
+    "id": 28,
+    "title": "Frontend Developer",
+    "location": "Noida, India",
+    "salary": "Rs. 8,00,000"
+  },
+  {
+    "id": 96,
+    "title": "DevOps Engineer",
+    "location": "Pune, India",
+    "salary": "Rs. 12,00,000"
+  }
+]
+
 
 @app.route("/")
 def home():
-  return render_template(template_name_or_list='home.html')
+  return render_template(template_name_or_list='home.html', openings=openings)
 
 
 if __name__ == "__main__":
