eb723fad1e26a59e7f278ca320fe14d911f1ab8b
Rajeshwar Sharma <62470446+me-rsharma@users.noreply.github.com>
2024-03-19 16:46:39 +0000

    Modify the style of sections and added links

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -28,6 +28,10 @@ openings = [
     "salary": "Bhagawat Krupa"
   }
 ]
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(openings)
 
 
 @app.route("/")
